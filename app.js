d74f11a30a9899a8372a7e2dcfdaef96c8fce36e
Animesh Raghav <95857840+animeshraghav@users.noreply.github.com>
2023-09-30 22:29:07 +0530

    updated

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
-
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
-
 function clearEntry() {
     const currentValue = document.getElementById('display').value;
     document.getElementById('display').value = currentValue.slice(0, -1);
 }
-
 function calculateResult() {
     const input = document.getElementById('display').value;
     try {
@@ -20,15 +17,11 @@ function calculateResult() {
         document.getElementById('display').value = 'Error';
     }
 }
-
 function evaluateExpression(expression) {
     return new Function('return ' + expression)();
 }
-
-// Keyboard support
 document.addEventListener('keydown', function (event) {
     const key = event.key;
-
     if (/[0-9+\-*/.=]|Enter|Backspace|Escape|Delete/.test(key)) {
         event.preventDefault();
         switch (key) {
